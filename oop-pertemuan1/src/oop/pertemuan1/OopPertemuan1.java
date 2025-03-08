6ada023d55d9564933f078c97888520c1d459771
urfavteddybear <business.urfavteddybear@gmail.com>
2025-03-08 12:19:00 +0800

    feat: Latihan 1

---
+++ oop-pertemuan1/src/oop/pertemuan1/OopPertemuan1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop.pertemuan1;
+
+/**
+ *
+ * @author I Gede Made Sasmita Wahyu Putra
+ * TGL: 8 Maret 2025
+ */
+public class OopPertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Menampilkan Tulisan di layar
+        System.out.print("Hello World!");
+    }
+    
+}
